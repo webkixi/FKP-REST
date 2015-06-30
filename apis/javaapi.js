3d7ee1d39c6d7bad19c9d05f4fadafdeadbf6cf8
ralf <kixi@163.com>
2015-06-30 15:34:15 +0800

    javaapi Signed-off-by: ralf <kixi@163.com>

--- apis/javaapi.js
+++ apis/javaapi.js
@@ -1,20 +1,39 @@
 // var request = require('request');
 var request = require('koa-request');
+var path = require('path');
+var libs = require('../libs/libs');
 
+var base = 'http://120.25.223.175:5051/jh-web-portal/';
+
+var req = function(api,options){
+    function rp(err, rep, body){   //deal with response result
+        if(error)
+            throw new Error("async search: no respons data");
+
+        if (!error && response.statusCode == 200)
+            return body;   // console.log(body) // Show the HTML for the Google homepage.
+    }
+    return function(rp)
+        request(api,rp);
+}
 
 module.exports = {
-    api: {
-
-    },
     req：request,
-    get:{
-        user:  getUser,
-        info:  getInfo,
-        infoList:  getInfo,
-        goods: getGood,
-        article: getArticle
-    }
+    search: getSearch,
+    info: getInfo,
+    goods: getGood,
+    article: getArticle
 }
+
+function *getSearch(param){
+    if(libs.getObjType(param)!=='Object')
+        return false;
+    // var uri = JSON.stringify(url);
+    var searchUrl = path.join(base,'search-json.html');
+    // st=1&sc=%E7%BD%91'
+    var apiData = yield req(searchUrl,{st:1,sc:'%E7%BD%91'});
+}
+
 
 function *getUser(url){
 
