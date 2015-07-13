e33b30cd1af493cb3f8c13fa43dbc15b14071112
lgh <lgh@qq.com>
2015-07-13 18:19:01 +0800

    login

--- pages/account/login.js
+++ pages/account/login.js
@@ -25,9 +25,32 @@ function *demoLoginData(oridata){
         // console.log(userInfo); 
 
         var body = yield libs.$parse(this); 
-        console.log(body);
         //apiData = yield api.xxx('loginCheck',body);
-        apiData = yield api.pullApiData('loginCheck',body);
+        if(body.loginPhone || body.password){
+            var mobilepartn = /^(((13[0-9]{1})|(14[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+            if(body.loginPhone=="" || body.loginPhone.length != 11 || !mobilepartn.test(body.loginPhone) || body.password=="" || body.password.length < 6){
+                oridata.errmsg = "手机或密码格式错误";
+            }else{
+                //检测传进来的数据是不是合法的
+                //......
+                apiData = yield api.pullApiData('loginCheck',body);
+                var jsonData = JSON.parse(apiData[1]);
+                console.log(jsonData);
+                if(jsonData.success){
+                    //数据在java这边是不是存在的
+                    //.....
+                    // console.log(jsonData.data);
+                    oridata.success = '登录成功!';
+                    console.log(oridata.static);
+                    //返回数据
+                    //.....
+                }else{
+                    oridata.errmsg = jsonData.errMsg;
+                };
+            }
+        }else{
+            oridata.errmsg = 'xxxxx';
+        }
     }
     return oridata;
 }
