a557ddce35466b04eaef74a83c45adafb8b014a4
laiwenbiao <laiwenbiao@qq.com>
2015-07-13 17:54:28 +0800

    公共头部导航对接完成

--- react/header_nav.jsx
+++ react/header_nav.jsx
@@ -5,14 +5,39 @@ var tmpApp = React.createClass({
 	
 	loopRender:function(){
 		var items = [];
-
+		var datas = this.props.data;
+		datas.menuList.map(function(item,i){
+			var active = "";
+			if(i == 0) active= "active home";
+			if(item.hasChildren){
+			var children = [];
+				item.children.map(function(childrenNav){
+					children.push(
+						<li><a href={"/"+childrenNav.menuUrl}>{childrenNav.menuName}</a></li>
+						)
+				})
+				items.push(
+					<li className={active}>
+						<a href={"/"+item.menuUrl}>{item.menuName}</a>
+						<ul className={"dropdown_menus"}>{children}</ul>
+					</li>
+				)
+			}else{
+				items.push(
+					<li className={active}>
+						<a href={"/"+item.menuUrl}>{item.menuName}</a>
+					</li>
+				)
+			}
+			
+		})
 		return items;
 	},
 
 	render: function () {
 		var fills = this.loopRender();
 		return (
-			<ul className={"info-mlist"}>
+			<ul className={"container clearfix"}>
 				{fills}
 			</ul>
 		)
