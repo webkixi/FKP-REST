16142cb0228d2863ab0febd568ba650f5c574c9f
yaocheng <yaocheng@163.com>
2015-07-14 16:49:53 +0800

    前段使用的API库

---
+++ public/src/pc/js/pages/_common/api.js
@@ -0,0 +1,29 @@
+var qs = require('querystring');
+var src = "http://120.25.223.175:5051/jh-web-portal/";
+var apiPath = {
+    base: src,
+    dirs: {
+        search: src+'api/search.html',
+        user: src+'checkUserStatus.html',
+        mall_list: src+'api/mall/item/list/query.html',
+        mall_attr: src+'api/mall/item/list/attributes.html'
+    }
+}
+
+function req(api,param,cb){
+    var url = apiPath.dirs[api];
+    var query = qs.stringify(param);
+
+    if(libs.getObjType(param)!=='Object')
+        return false;
+	request({method:'POST', url:url+'?'+query, json:{relaxed:true}}, function(err,response,body){
+		if(err)
+		    throw err
+        cb.call(null,body);
+	});
+}
+
+module.exports = {
+	apiPath: apiPath,
+	req: req
+}
