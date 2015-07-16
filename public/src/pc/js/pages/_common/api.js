77004b1ed2c1e34434eebc1e269235b2093be77f
yaocheng <yaocheng@163.com>
2015-07-16 23:08:15 +0800

    优化静态api提交方法

--- public/src/pc/js/pages/_common/api.js
+++ public/src/pc/js/pages/_common/api.js
@@ -7,17 +7,23 @@ var apiPath = {
         search: src+'api/search.html',
         user: src+'checkUserStatus.html',
         mall_list: src+'api/mall/item/list/query.html',
-        mall_attr: src+'api/mall/item/list/attributes.html'
+        mall_attr: src+'api/mall/item/list/attributes.html',
+        login: './login'
     }
 }
 
 function req(api,param,cb){
+    console.log('11111111111111111');
+    console.log(param);
+    param.relaxed = true;
     var url = apiPath.dirs[api];
     var query = qs.stringify(param);
 
     if(libs.getObjType(param)!=='Object')
         return false;
-	request({method:'POST', url:url+'?'+query, json:{relaxed:true}}, function(err,response,body){
+	request({method:'POST', url:url, json:param}, function(err,response,body){
+	// request({method:'POST', url:url+'?'+query, json:{relaxed:true}}, function(err,response,body){
+	// request({method:'POST', url:url+'?'+query}, function(err,response,body){
 		if(err)
 		    throw err
         cb.call(null,body);
