81c2c67d18cc9c968f1489385abff7a0af201a1e
laiwenbiao <laiwenbiao@qq.com>
2015-08-03 13:50:04 +0800

    修改注册也样式 Signed-off-by: laiwenbiao <laiwenbiao@qq.com>

--- react/info_side_nav.jsx
+++ react/info_side_nav.jsx
@@ -25,7 +25,7 @@ var tmpApp = React.createClass({
 						<dt>
 							<a href={"/info/cat/"+item.id+".html"}><i></i>{item.catName}</a>
 						</dt>
-						<dd>{dds}</dd>
+						{dds}
 						</dl>
 					</li>
 				)
