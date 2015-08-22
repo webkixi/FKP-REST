0187535e55b9339cbfaf14a5d05a46abd6e7824d
yaocheng <yaocheng@163.com>
2015-08-22 18:52:15 +0800

    co-wechat

--- modules/route.js
+++ modules/route.js
@@ -124,7 +124,7 @@ function init(app,mapper,rend){
             yield captcha.call(this);
         }else if(param.cat === 'weixin'){
             console.log('aaaaaaaaaaa');
-            yield wechat.call(this,app);
+            yield wexin.call(this,app);
         }
         else
             yield distribute.call(this,mapper)
@@ -181,50 +181,49 @@ function *uploader(){
     // }
 }
 
-function *wechat(app){
+function *weixin(app){
     libs.clog('微信')
-    console.log(this.req.body);
-    // app.use(wechat(wx_config2).middleware(function *() {
-    //     console.log('yyyyyyyyyyyyyyyyy');
-    //     var message = this.weixin;
-    //       if (message.FromUserName === 'diaosi') {
-    //         // 回复屌丝(普通回复)
-    //         this.body = 'hehe';
-    //       } else if (message.FromUserName === 'text') {
-    //         //你也可以这样回复text类型的信息
-    //         this.body = {
-    //           content: 'text object',
-    //           type: 'text'
-    //         };
-    //       } else if (message.FromUserName === 'hehe') {
-    //         // 回复一段音乐
-    //         this.body = {
-    //           type: "music",
-    //           content: {
-    //             title: "来段音乐吧",
-    //             description: "一无所有",
-    //             musicUrl: "http://mp3.com/xx.mp3",
-    //             hqMusicUrl: "http://mp3.com/xx.mp3"
-    //           }
-    //         };
-    //       } else if (message.FromUserName === 'kf') {
-    //         // 转发到客服接口
-    //         this.body = {
-    //           type: "customerService",
-    //           kfAccount: "test1@test"
-    //         };
-    //       } else {
-    //         // 回复高富帅(图文回复)
-    //         this.body = [
-    //           {
-    //             title: '你来我家接我吧',
-    //             description: '这是女神与高富帅之间的对话',
-    //             picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-    //             url: 'http://nodeapi.cloudfoundry.com/'
-    //           }
-    //         ];
-    //       }
-    // }))
+    app.use(wechat(wx_config2).middleware(function *() {
+        console.log('yyyyyyyyyyyyyyyyy');
+        var message = this.weixin;
+          if (message.FromUserName === 'diaosi') {
+            // 回复屌丝(普通回复)
+            this.body = 'hehe';
+          } else if (message.FromUserName === 'text') {
+            //你也可以这样回复text类型的信息
+            this.body = {
+              content: 'text object',
+              type: 'text'
+            };
+          } else if (message.FromUserName === 'hehe') {
+            // 回复一段音乐
+            this.body = {
+              type: "music",
+              content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3"
+              }
+            };
+          } else if (message.FromUserName === 'kf') {
+            // 转发到客服接口
+            this.body = {
+              type: "customerService",
+              kfAccount: "test1@test"
+            };
+          } else {
+            // 回复高富帅(图文回复)
+            this.body = [
+              {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+              }
+            ];
+          }
+    }))
 }
 
 //上传数据
