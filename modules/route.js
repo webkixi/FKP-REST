d1b8655ef7892ad6b694f85c88548f9de0b5aca6
yaocheng <yaocheng@163.com>
2015-08-22 17:44:39 +0800

    weixin develop

--- modules/route.js
+++ modules/route.js
@@ -13,6 +13,13 @@ var region = require('./region');
 var config = require('../config');
 // require('jsx-require-extension/options/harmony');   //另一套方案 node-jsx
 
+var wechat = require('co-wechat')
+var wx_config = {
+  token: 'agzgz',
+  appid: 'wxc9edcce4f4d915e6',
+  encodingAESKey: 'a4JE5P7ImZWnU6UpLVgr6uldgrthwiwkweo8LTapZmC'
+};
+
 
 
 /**
@@ -108,6 +115,8 @@ function init(app,mapper,rend){
             yield uploader.call(this);
         }else if(param.cat === 'captcha'){
             yield captcha.call(this);
+        }else if(param.cat === 'weixin'){
+            yield wechat.call(this,app);
         }
         else
             yield distribute.call(this,mapper)
@@ -162,6 +171,50 @@ function *uploader(){
     //     var error = { success: false}
     //     this.body = JSON.stringify(error)
     // }
+}
+
+function *wechat(app){
+    libs.clog('微信')
+    app.use(wechat('agzgz').middleware(function *() {
+        var message = this.weixin;
+          if (message.FromUserName === 'diaosi') {
+            // 回复屌丝(普通回复)
+            this.body = 'hehe';
+          } else if (message.FromUserName === 'text') {
+            //你也可以这样回复text类型的信息
+            this.body = {
+              content: 'text object',
+              type: 'text'
+            };
+          } else if (message.FromUserName === 'hehe') {
+            // 回复一段音乐
+            this.body = {
+              type: "music",
+              content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3"
+              }
+            };
+          } else if (message.FromUserName === 'kf') {
+            // 转发到客服接口
+            this.body = {
+              type: "customerService",
+              kfAccount: "test1@test"
+            };
+          } else {
+            // 回复高富帅(图文回复)
+            this.body = [
+              {
+                title: '你来我家接我吧',
+                description: '这是女神与高富帅之间的对话',
+                picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+                url: 'http://nodeapi.cloudfoundry.com/'
+              }
+            ];
+          }
+    }))
 }
 
 //上传数据
