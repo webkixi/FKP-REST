12ff28eb2cd0b63b62aa8d80fa3daf670e3dd359
yaocheng <yaocheng@163.com>
2015-08-25 23:23:04 +0800

    tabswitch

--- public/src/pc/js/modules/tabs/like_jd.jsx
+++ public/src/pc/js/modules/tabs/like_jd.jsx
@@ -17,27 +17,35 @@ var render = React.render;
 function tabswitch(tab_nav_data,tab_cnt_data,ele,opts){
     var tabItemMethod = function(){
         $(this).hover(function(e){
-            $("#"+ele+" .tab-uls").show();
+            $("#"+ele+" .seleCnt").show();
             $(this).addClass('active').siblings().removeClass("active");
             var idf = $(this).attr('data-idf');
             SA.setter('Jd_uls', { data: tab_cnt_data[idf] })
         },function(){
-            $("#"+ele+" .tab-uls").hide();
+            $("#"+ele+" .seleCnt").hide();
         });
-        $("#"+ele+" .tab-uls").hover(function(){
+        $("#"+ele+" .seleCnt").hover(function(){
             $(this).show();
         },function(){
             $(this).hide();
         });
         $(".tabswitch").hover(function(){
         },function(){
-            $("#"+ele+" .tab-uls").prev(".vlist").children("li").removeClass("active");
+            $("#"+ele+" .seleCnt").prev(".vlist").children("li").removeClass("active");
         })
     }
 
     render(
         <Tabswitch data={tab_nav_data} listClass={'vlist tiger2'} itemDefaultMethod={tabItemMethod} itemClass={'xxx'} itemIcon={'true'}>
-            <Uls data={tab_cnt_data} listClass={'vlist fox'} itemClass={'xxx'}/>
+            <div style={{display:'none'}} className={'seleCnt'}>
+                <div style={{display: 'inline-block',width:'80%'}}>
+                    <div style={{backgroundColor:'#efefef',height:'60px',marginLeft:'210px'}}>{'12345'}</div>
+                    <Uls data={tab_cnt_data} listClass={'vlist fox'} itemClass={'xxx'}/>
+                </div>
+                <div style={{display: 'inline-block',width:'20%',backgroundColor:'red',minHeight:'100%'}}>
+                    {'abc'}
+                </div>
+            </div>
         </Tabswitch>
         ,document.getElementById(ele)
     )
