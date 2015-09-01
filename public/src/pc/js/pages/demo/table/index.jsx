d0f4e1a3bda89b995eebcd617b99930a91729ec4
laiwenbiao <laiwenbiao@qq.com>
2015-09-01 22:38:58 +0800

    表格demo页加入分页 Signed-off-by: laiwenbiao <laiwenbiao@qq.com>

--- public/src/pc/js/pages/demo/table/index.jsx
+++ public/src/pc/js/pages/demo/table/index.jsx
@@ -1,5 +1,7 @@
 var table = require('modules/table/table');
 
+//引入模块
+var pagination = require('modules/pagination/pagi');\
 // var data = require('../_json/mall_list.json')
 
 //表格
@@ -33,3 +35,21 @@ var tab_body_data=[
 // 表格
 // Tabswitch( '表格数据', '表头数据', '表格宽度',  '页面容器id' )
 table( tab_body_data, tab_head, tab_head_width, "tab-switch-table" )
+
+
+// 根据总的文章数和每页文章数算出分页总数，并配好链接
+var pagi = {
+        total: 300,   //文章总数
+        per:   10,    //每页文章数
+        url:   '/xxx/yyy.html',  //文章url
+        query: 'abc="slime"&xyz="pack"&curentPage='  //文章query
+    },
+
+    //分页条有几个分页标签
+    begin = {
+        start: 0,
+        off: 10
+    }
+
+
+pagination( pagi, begin, 'pagi')
