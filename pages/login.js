e1bf3faff2d0514520583f87bf75cc614e7bcd44
yao cheng <yaocheng@163.com>
2015-10-29 12:23:05 +0800

    微信数据对接

--- pages/login.js
+++ pages/login.js
@@ -42,8 +42,10 @@ function *demoIndexData(oridata){
             var logindata = yield api.pullApiData('login', postdata, 'post')
             console.log('ooooooooo for openid user ooooooooo');
             console.log(logindata[1]);
-            console.log(logindata[1].results[0].addr);
-            console.log(logindata[1].results[0].usercar);
+            if(logindata[1].code==1){
+                console.log(logindata[1].results[0].addr);
+                console.log(logindata[1].results[0].usercar);                 
+            }
             if (logindata)
                 return logindata[1];
 
--- public/src/pc/js/pages/_common/weixin.js
+++ public/src/pc/js/pages/_common/weixin.js
@@ -48,6 +48,18 @@ var test_wx_data = {
     "privilege": []
 }
 
+var test_wx_data1 = {
+    "openid": "o07NUs250UkhoK8Ks6bZAZK7Hkls",
+    "nickname": "天天修改",
+    "sex": 1,
+    "language": "zh_CN",
+    "city": "广州",
+    "province": "广东",
+    "country": "中国",
+    "headimgurl": "http://wx.qlogo.cn/mmopen/Lhlz6ia774dNwUgAucLtKIs94BWFSX1hPbdvibZT79y69oI7FusBz7I5qbech1olibVxribJ9vFErEoDrJFPpDa0my6yXkhmouoj/0",
+    "privilege": []
+}
+
 function init_wx(cb){
     // SA.setter("_LOCAL_USER", tes_data.results[0]);
     // SA.setter("_WEIXIN", {user: test_wx_data})
