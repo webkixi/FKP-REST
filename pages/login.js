34cd2d6f115a9e630fbd8f6736280c4e778ea458
yao cheng <yaocheng@163.com>
2015-11-03 15:02:06 +0800

    服务端pages/login

--- pages/login.js
+++ pages/login.js
@@ -41,10 +41,10 @@ function *demoIndexData(oridata){
 
             var logindata = yield api.pullApiData('login', postdata, 'post')
             console.log('ooooooooo for openid user ooooooooo');
-            console.log(logindata[1]);
-            if(logindata && logindata[1].code==1){
+            console.log(logindata);
+            if(logindata && logindata[1] && logindata[1].code==1){
                 console.log(logindata[1].results[0].addr);
-                console.log(logindata[1].results[0].usercar);                 
+                console.log(logindata[1].results[0].usercar);
             }
             if (logindata)
                 return logindata[1];
