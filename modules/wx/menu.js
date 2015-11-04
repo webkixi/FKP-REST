f083c3686c74fb9e2630b1250611462d6d2027a5
yao cheng <yaocheng@163.com>
2015-11-05 04:20:20 +0800

    微信菜单

--- modules/wx/menu.js
+++ modules/wx/menu.js
@@ -8,63 +8,13 @@ var json = {
         "name": "服务",
         "type": "view",
         "url": "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+config.weixin.appid+"&redirect_uri=http%3A%2F%2Fch.dabai360.com&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect"
-        // "sub_button":
-        //     [
-        //         {
-        //            "type":"click",
-        //            "name":"即时新闻",
-        //            "key":"jsxw"
-        //         },
-        //         {
-        //            "type":"click",
-        //            "name":"财经早餐",
-        //            "key":"cjzc"
-        //         },
-        //         {
-        //            "type":"click",
-        //            "name":"焦点话题",
-        //            "key":"jdht"
-        //         },
-        //         {
-        //            "type":"view",
-        //            "name":"财经日历",
-        //            "url":"http://stock1.sina.cn/dpool/stockv2/universal_calendar.php?vt=4"
-        //         }
-        //     ]
         },
 
 	   {
            "name": "河马汽车",
            "type": "view",
            "url": "http://ch.dabai360.com/about.html"
-        //    "sub_button":[
-        //     {
-        //        "type":"click",
-        //        "name":"今日看盘",
-        //        "key":"jrkp"
-        //     },
-        //     {
-        //        "type":"view",
-        //        "name":"行情刷新",
-        //        "url":"http://XX/index.php/market/index"
-        //     },
-		// 	{
-        //        "type":"click",
-        //        "name":"交易策略",
-        //        "key":"jycl"
-        //     },
-		// 	{
-        //        "type":"click",
-        //        "name":"投资天地",
-        //        "key":"tztd"
-        //     },
-		// 	{
-        //        "type":"click",
-        //        "name":"投资文库",
-        //        "key":"tzwk"
-        //     }]
        },
-
 	   {
            "name":"我",
            "sub_button":[
