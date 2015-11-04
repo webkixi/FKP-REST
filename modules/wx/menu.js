9d4ee7fbfcc31dc347b23644ed1143accae0ae1d
yao cheng <yaocheng@163.com>
2015-11-05 03:53:26 +0800

    微信菜单

--- modules/wx/menu.js
+++ modules/wx/menu.js
@@ -95,6 +95,8 @@ function *query(){
 
 function *create(){
     var now_m = yield query.call(this)
+    console.log('-------- create menu ----------');
+    console.log(now_m);
     if(now_m.errcode>0){
         var nowmenu = yield api.pullWxData.call(this,'createmenu',{ body:json }, 'post' );
         console.log(nowmenu[0].body);
