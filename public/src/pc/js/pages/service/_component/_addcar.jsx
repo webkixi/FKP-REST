f839d3b57fffc52ee313d43660e80069d54f6304
yao cheng <yaocheng@163.com>
2015-11-30 21:32:53 +0800

    选择车系的问题

--- public/src/pc/js/pages/service/_component/_addcar.jsx
+++ public/src/pc/js/pages/service/_component/_addcar.jsx
@@ -89,8 +89,8 @@ function dealWith_Data_Series(){
     var rtnDom;
     var nav = [];
     // _car.model.empty();
-    // pn = { carbrand: $("#brand").find("input").val()}
-    pn = { carbrand: _car.brand.text}
+    pn = { carbrand: $("#brand").find("input").val()}
+    // pn = { carbrand: _car.brand.text}
     SA.setter('Pop',{data:{display:'block'}})
     api.req('queryseries',pn, function(data){
       if(data.code && data.code===1){
