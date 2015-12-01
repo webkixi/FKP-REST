1d36c2a1448ee438726754a56bc9c97111d5dff6
yao cheng <yaocheng@163.com>
2015-12-01 15:40:52 +0800

    api优化

--- public/src/pc/js/libs/api.js
+++ public/src/pc/js/libs/api.js
@@ -6,40 +6,7 @@ var demoSrc = "http://mock.agzgz.com/";
 var apiPath = {
     base: src,
     dirs: {
-        // service: src+'service',   //小保养
-        // parts: src+'parts',   //小保养
-        // queryallbrand: src+'queryallbrand'   //汽车品牌
-        // queryseries: src+'queryseries',   //汽车品牌
-        // querycartype: src+'querycartype'   //汽车品牌
-
-
-        // ,region: src+'region'   //获取地区信息
-        // ,getmms: src+'getmms'   //获取短信验证码
-        // ,getservtime: src+'getservtime' //获取服务器时间
-
-        // ,order: src+'order' //提交订单
-        // ,payment: src+'payment' //支付
-
-        // ,mobilecode: src+'mobilecode'   //注册
-
-        // ,useraddcar: src+'useraddcar'   //添加车辆
-        // ,mycarlist: src+'mycarlist'     //我的车列表
-        // ,mycar_del: src+'mycar_del'     //删除车辆
-
-        // ,order_addr: src+'order_addr'
-        // ,order_addaddr: src+'order_addaddr'
-        // ,order_deladdr: src+'order_deladdr'
-
-        // ,washcar: src+'washcar'
-
-        // ,login: src+'login'   //获取登陆用户信息
-        // ,carchecking: src+'carchecking'   //订单列表
-
-        // ,getshoplist: src+'getshoplist'
-        // ,discountlist: src+'discountlist'
-
-        // ,myorder_info: src+'myorder_info'  //订单详情
-        // ,order_list: src+'order_list'  //订单列表
+        
     },
     weixin: {
         userlist: src+'wx/userlist',   //?access_token=_cqch&next_openid=
