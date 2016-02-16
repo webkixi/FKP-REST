ce07ac469cbf6a63a8dae1b004cd5d1e70be1248
yao cheng <yaocheng@163.com>
2016-02-16 11:34:13 +0800

    node端，错误模块定义

---
+++ libs/errors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    '10000': {error: '10000', message: "ok"},
+
+    // models/user.js
+    "10001": {error: "10001", message: "user not found"},
+    "10002": {error: "10002", message: "password dose not match"},
+    "10003": {error: "10003", message: "该用户名已被注册"},
+    "10004": {error: "10004", message: "两次密码不匹配"},
+    "10005": {error: "10005", message: "请登录"}
+}
