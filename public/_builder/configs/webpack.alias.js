bbaa8aa39a146c3d9630fca344a59205f279b448
yao cheng <yaocheng@163.com>
2016-02-18 00:35:44 +0800

    修改react组件及模块的路径配置文件

--- public/_builder/configs/webpack.alias.js
+++ public/_builder/configs/webpack.alias.js
@@ -9,16 +9,17 @@ var configs = require('./config');
 module.exports = {
     'pages': path.resolve(path.join(__dirname, '../../', configs.dirs.pages)),
     'vendor': path.resolve(path.join(__dirname, '../../', configs.dirs.vendor)),
-    'modules': path.resolve(path.join(__dirname, '../../', configs.dirs.modules)),
+    // 'modules': path.resolve(path.join(__dirname, '../../', configs.dirs.modules)),
+    'modules': path.resolve(path.join(__dirname, '../../react/modules')),
     'widgets': path.resolve(path.join(__dirname, '../../', configs.dirs.widgets)),
-    'stores': path.resolve(path.join(__dirname, '../../', configs.dirs.stores)),
-    'mixins': path.resolve(path.join(__dirname, '../../', configs.dirs.mixins)),
+    'widgets': path.resolve(path.join(__dirname, '../../react/widgets')),
+    // 'mixins': path.resolve(path.join(__dirname, '../../', configs.dirs.mixins)),
+    'mixins': path.resolve(path.join(__dirname, '../../react/mixins')),
     'libs': path.resolve(path.join(__dirname, '../../', configs.dirs.libs)),
 
-
     // 组件
-    'Swipe': path.resolve('modules/swipejs/swipe'),
+    // 'Swipe': path.resolve('modules/swipejs/swipe'),
 
     // 模块
-    'Sp': path.resolve('modules/sp/sp'),
+    // 'Sp': path.resolve('modules/sp/sp'),
 };
