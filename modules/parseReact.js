32dc6531bd3962c31cfe8527198fbdefcda92529
yao cheng <yaocheng@163.com>
2016-02-18 16:44:06 +0800

    优化了node端react2html模块

--- modules/parseReact.js
+++ modules/parseReact.js
@@ -1,13 +1,14 @@
-var React = require('react/addons');
-require('jsx-require-extension/options/harmony');   //另一套方案 node-jsx
-// require('node-jsx').install({harmony: true})
 
-
-var base = '../react/';
-
-module.exports = function(url, opt){
-	props = opt || {};
-	var ReactApp = React.createFactory(require(base+url));
-	var reactHtml = React.renderToString(ReactApp(props));
-	return reactHtml;
+module.exports = function(reacturl, opt){
+	var factory;
+	var props = opt || {};
+	if (typeof reacturl==='string'){
+		factory = React.createFactory(include(reacturl));
+	}
+	else
+	if (typeof reacturl==='object'){
+		factory = reacturl
+	}
+	var reactHtml = React.renderToString(factory(props));
+	return [reactHtml];
 }
