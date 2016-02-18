bdd5fd36aff74d3ef550b762d8f2c30febc1763e
yao cheng <yaocheng@163.com>
2016-02-18 16:45:52 +0800

    示例，完成前端，后端公用react组件

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+var api = include('apis/javaapi')
+var react2html = include('modules/parseReact')
+
 function *index(oridata) {
-    var api = this.include('apis/javaapi')
 
     var method = this.method;
 
     if (method === 'GET') {
-        oridata.fkp = 'FKP-REST'
-        var kkk = yield api.req(this, '$listtopic')
-        console.log('======= kkk ========');
-        console.log('======= kkk ========');
-        console.log('======= kkk ========');
-        console.log(kkk);
+
+        //从数据库中获取数据
+        // '$' 为数据库api的前置标记，与前端一致
+        var listdata_ori = yield api.req(this, '$listtopic')
+
+        //整理数据，使数据符合 react 需求
+        var listdata_targ = []
+        listdata_ori.map(function(item){
+            listdata_targ.push(item.title)
+        })
+
+        // react格式化为html结构
+        // 传入 react 组件路径及数据
+        var reactHtml = yield react2html('react/listView/list', {data: listdata_targ})
+        console.log(reactHtml[0]);
+
+        // 将html结构代码代入到oridata，用于模板渲染
+        oridata.bloglist = reactHtml[0]
         return oridata;
     }
 
