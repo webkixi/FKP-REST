131f949c9bfe538fed623749ecf1b9900e8fb73d
姚成 <kixi@163.com>
2016-04-27 13:15:37 +0800

    增加tablist load的demo

---
+++ public/src/pc/js/pages/cat/_component/uc/order.jsx
@@ -0,0 +1,128 @@
+var router = require('libs/router').router;
+var pages = require('libs/pages');
+var ItemMixin = require('mixins/item');
+var libs = require('libs/libs');
+var api = libs.api;
+var _user = SA.get('USER')
+var valide = libs.formValide;
+var VTabs = require('modules/list/tabs_list');
+
+var _page = {}
+
+
+//当前页公共变量
+var _page = {}
+
+var menus = [
+    // {id: '111', key: "abc", title: "苹果", 'data-api': "/xxx?id=111"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '111', key: "abc", title: "苹果"},
+    {id: '112', key: "bcd", title: "三星"},
+    {id: '113', key: "cde", title: "华为"},
+    {id: '114', key: "rrr", title: "小米"}
+]
+
+var lists = [
+    {id: '111', key: "abc", title: "苹果的手机", url: "http://www.163.com"},
+    {id: '112', key: "bcd", title: "三星的手机", url: "http://www.163.com"},
+    {id: '113', key: "cde", title: "华为的手机", url: "http://www.163.com"},
+    {id: '114', key: "rrr", title: "小米的手机", url: "http://www.163.com"},
+    {id: '111', key: "abc", title: "苹果的手机", url: "http://www.163.com"},
+    {id: '112', key: "bcd", title: "三星的手机", url: "http://www.163.com"},
+    {id: '113', key: "cde", title: "华为的手机", url: "http://www.163.com"},
+    {id: '114', key: "rrr", title: "小米的手机", url: "http://www.163.com"},
+    {id: '111', key: "abc", title: "苹果的手机", url: "http://www.163.com"},
+    {id: '112', key: "bcd", title: "三星的手机", url: "http://www.163.com"},
+    {id: '113', key: "cde", title: "华为的手机", url: "http://www.163.com"},
+    {id: '114', key: "rrr", title: "小米的手机", url: "http://www.163.com"},
+    {id: '111', key: "abc", title: "苹果的手机", url: "http://www.163.com"},
+    {id: '112', key: "bcd", title: "三星的手机", url: "http://www.163.com"},
+    {id: '113', key: "cde", title: "华为的手机", url: "http://www.163.com"},
+    {id: '114', key: "rrr", title: "小米的手机", url: "http://www.163.com"},
+    {id: '111', key: "abc", title: "苹果的手机", url: "http://www.163.com"},
+    {id: '112', key: "bcd", title: "三星的手机", url: "http://www.163.com"},
+    {id: '113', key: "cde", title: "华为的手机", url: "http://www.163.com"},
+    {id: '114', key: "rrr", title: "小米的手机", url: "http://www.163.com"}
+]
+
+var menu_funs = {
+    item: function(){
+        $(this).click(function(){
+            alert(123)
+        })
+    },
+    scrollEnd: function(_name, _next){
+        alert('333')
+    }
+}
+
+var list_funs = {
+    item: function(){
+        $(this).click(function(){
+            alert(456)
+        })
+    },
+    scrollEnd: function(_name, _next){
+        alert('666')
+    }
+}
+
+
+
+function start(name){
+    return pages.new({
+        boot: function(self){
+            api.req('/myOrderList', {login: _user.login}, function(data){
+                console.log('/myOrderList ------------- ');
+                console.log(data);
+                _page = {
+                    data: data
+                }
+            })
+        },
+        trigger:function(){
+            this.libs.changeTitle('我的订单');    //更改当前页面标题
+            this.main()
+        },
+        ready: function(){
+        },
+        main: function(self, data){
+            VTabs(menus, lists, {
+                container: name,
+                itemMethod: {
+                    menu: menu_funs.item,
+                    list: list_funs.item
+                },
+                scrollEnd: {
+                    menu: menu_funs.scrollEnd,
+                    list: list_funs.scrollEnd
+                }
+            })
+        },
+        end: function(){
+            // React.unmountComponentAtNode(document.getElementById(name))             
+        }
+    })
+}
+
+module.exports = start;
---
+++ public/src/pc/js/pages/cat/index.jsx
@@ -0,0 +1,30 @@
+var libs = require('libs/libs');
+var router = require('libs/router').router;
+var route = require('libs/router').route;
+var url = libs.urlparse(location.href);
+SA.set('USER', {login: false})
+var api = libs.api;
+
+if (url.params.login){
+    SA.set('USER', {login: url.params.login})
+    api.req('/userInfo', {login: url.params.login, method: 'get'}, function(data){
+        SA.append('USER', {info: data})
+    })
+}
+
+
+//添加自己的路由
+route.init({
+    'uc/order':require('./_component/uc/order'),            //订单
+});
+
+if(url.params.hash){
+    var hash = url.params.hash
+    router(hash)
+}else{
+    if (url.hash){
+        router(url.hash)
+    }
+    else
+        router('uc/order')   //指定默认首页
+}
