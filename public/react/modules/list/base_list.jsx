64582bdcaf4b3d06257f97add922cc22afaf80a0
姚成 <kixi@163.com>
2016-06-02 01:11:14 +0800

    单列react模块

--- public/react/modules/list/base_list.jsx
+++ public/react/modules/list/base_list.jsx
@@ -1,8 +1,7 @@
 /**
- * 左边是导航部分
- * 右边列表部分
- * 现在比较流行的一种ui方式
+ * 单列带自动加载列表
  */
+
 var libs = require('libs/libs'),
     List = require('./_component/_loadlist'), //设定列表域为 lagou
     ItemMixin = require('mixins/item'),
@@ -11,8 +10,10 @@ var libs = require('libs/libs'),
     req = libs.api.req,
     _ = libs.lodash;
 
-//注入css
-inject.css(['/css/t/list/tabslist.css', 'tabslist']);
+
+//注入like_lagou的样式
+inject.css(['/css/t/list/lagou.css', 'like_lagou']);
+// inject.js(['/js/t/jq/draggabilly.pkgd.min.js', 'draggabilly']);
 
 function doneNext(eve){
     var me = this;
@@ -25,7 +26,9 @@ function doneNext(eve){
                 throw 'must specify ddd, ddd is Array type'
 
             if (!ddd.length){
-              $(me).find('li[data-cls="loadbar"]').html('没有更多主题可看了')
+              setTimeout(function(){
+                  $(me).find('.loadtype').parent().css('display','none')
+              }, 5000)
               return false
             }
 
@@ -48,89 +51,57 @@ function doneNext(eve){
     }
 }
 
-function applist(m, l, ele, opts){
+function applist(data, ele, opts){
+
     var noop = function(){},
         dft = {
             container: '',
             trigger: 'auto',
-            globalName: {
-                menu: 'TabsMenus',
-                list: 'TabsLists'
-            },
-            itemMethod: {
-                menu: noop,
-                list: noop
-            },
-            listMethod:{
-                menu: noop,
-                list: noop
-            },
-            scroll: {
-                menu: noop,
-                list: noop
-            },
-            scrollEnd:{
-                menu: noop,
-                list: noop
-            },
-            empty: {
-                menu: <div>没有数据</div>,
-                list: <div>没有数据</div>
-            }
+            globalName: 'LagouLists',
+            itemMethod: noop,
+            listMethod: noop,
+            listClass: 'yyy',
+            scroll: noop,
+            scrollEnd: noop,
+            empty: <div>没有数据</div>
         }
 
     if (_.isObject(ele))
         opts = ele;
 
-
     dft = _.assign(dft, opts)
 
-    if (!dft.container){
-        console.log('tabs_list需要指定id');
-        return false
-    }
+    if (!dft.container) return false;
 
-    SA.set(dft.globalName.menu, {data: m})
-    SA.set(dft.globalName.list, {data: l})
+    SA.set(dft.globalName, {data: data})
+    // SA.set(dft.globalName.list, {data: l})
 
-    function xxx(type, sem){
-        var _name = dft.globalName[type]
+    function xxx(sem){
+        var _name = dft.globalName
+        // SA.set(_name, {data:[]})
         return function(top){
             var self = this;
             sem.call(self, _name, doneNext.call(self, dft.trigger))
         }
     }
 
-    for (var type in dft.scrollEnd){
-        dft.scrollEnd[type] = xxx(type, dft.scrollEnd[type])
-    }
+    dft.scrollEnd = xxx(dft.scrollEnd)
 
     // 定义列表
-    var MENUS = List(dft.globalName.menu),
-        LISTS = List(dft.globalName.list);
+    var LISTS = List(dft.globalName);
 
-
-    var data_menus = m,
-        data_lists = l;
-
+    var data_lists = data;
 
     var VTabs = React.createClass({
         mixins: [ItemMixin],
         getInitialState: function(){
             return {
-                menus: [],
                 lists: []
             }
         },
         componentWillMount: function(){
             if (this.props.data){
                 var data = this.props.data;
-                if (data.menus){
-                    this.setState({
-                        menus: data.menus
-                    })
-                }
-
                 if (data.lists){
                     this.setState({
                         lists: data.lists
@@ -139,39 +110,104 @@ function applist(m, l, ele, opts){
             }
         },
         render:function(){
-            var _this = this;
-            return (
-                <div className="tabslist">
-                    <div ref='tabs-menu' className="tabs-menu">
-                        {(function(){
-                            if (_this.state.menus && _this.state.menus.length){
-                                return <MENUS onscrollend={dft.scrollEnd.menu} scroll="self" data={_this.state.menus} itemMethod={dft.itemMethod.menu}/>
-                            }
-                            else {
-                                return dft.empty.menu
-                            }
-                        })()}
-                    </div>
-                    <div ref='tabs-list' className="tabs-list">
-                        {(function(){
-                            if (_this.state.lists && _this.state.lists.length){
-                                return <LISTS onscrollend={dft.scrollEnd.list} scroll="self" data={_this.state.lists} itemMethod={dft.itemMethod.list}/>
-                            }
-                            else {
-                                return dft.empty.list
-                            }
-                        })()}
-                    </div>
-                </div>
-            );
+            if (!this.state.lists.length){
+                return dft.empty
+            }
+            else {
+                return (
+                    <LISTS listClass={dft.listClass} onscrollend={dft.scrollEnd} scroll="self" data={this.state.lists} itemMethod={dft.itemMethod}/>
+                );
+            }
         }
     })
 
-    var data_render = {menus: data_menus, lists:data_lists}
+    var data_render = {lists:data_lists}
     render(
         <VTabs data={data_render} />,
         document.getElementById(dft.container)
     )
+
+
+
+
+
+
+
+
+
+
+
+    // var defaults = {
+    //     sem: function(){},   //scroll end method
+    //     evt: 'click',       // scroll end event
+    //     callback: function(){}  //after list rendered, bind some behavior on dom
+    // }
+    //
+    // if( opts && typeof opts==='object' ){
+    //     defaults = libs.extend({}, defaults, opts)
+    // }
+    //
+    //
+    // var lm = function(){};
+    // if (typeof defaults.callback === 'function'){
+    //     lm = defaults.callback
+    // }
+    //
+    // var scrollEndMethod = function(){
+    //     var me = this;
+    //     var td;
+    //     var tmpData = SA.getter('LAGOU')
+    //     if(!tmpData.data){
+    //         td = data;
+    //         SA.set('LAGOU', {data:data})
+    //     }
+    //
+    //     if (defaults.sem && typeof defaults.sem === 'function') {
+    //         defaults.sem(defaults.evt, doneNext)
+    //     }
+    //     else {
+    //         //load more content
+    //         doneNext(defaults.evt, td)
+    //     }
+    //
+    //     // refresh item's event of list
+    //     setTimeout(lm, 200)
+    //
+    //     //load more content function is click or auto-load
+    //     function doneNext(eve, ddd){
+    //         try {
+    //             if (!ddd || libs.type(ddd)!=='Array')
+    //                 throw 'must specify ddd, ddd is Array type'
+    //
+    //             if (!ddd.length){
+    //               $(me).find('li[data-cls="loadbar"]').html('没有更多主题可看了')
+    //               return false
+    //             }
+    //
+    //             var _data = SA.get('LAGOU').data
+    //             _data = _data.concat(ddd)
+    //
+    //             if(!eve || eve!=='auto'){
+    //                 $(me).find('li[data-cls="loadbar"]').click(function(){
+    //                     SA.setter('LAGOU', {data: _data});
+    //                 })
+    //             }
+    //             else
+    //             if(eve === 'auto'){
+    //                 SA.setter('LAGOU', {data: _data});
+    //             }
+    //
+    //         } catch (e) {
+    //             libs.msgtips('like_lagou :: doneNext: '+e, 'alert')
+    //         }
+    //     }
+    //
+    // }
+    //
+    // render(
+    //     <List data={data} onscrollend={scrollEndMethod} listMethod={lm} itemClass={'lg_item'} listClass={'like_lagou'} itemView={ITEM}/>,
+    //     document.getElementById(ele)
+    // )
 }
 
 module.exports = applist;
