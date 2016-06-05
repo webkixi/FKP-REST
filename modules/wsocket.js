38123361d428f4c922c52eb1cc0197cc6cdf5400
姚成 <kixi@163.com>
2016-06-06 01:52:07 +0800

    web socket模块优化，完成度90%

--- modules/wsocket.js
+++ modules/wsocket.js
@@ -1,31 +1,82 @@
-/**
- * websocket 模块
- */
-var koa = require('koa');
+var http = require('http'),
+    socket_config = {},
+    io,
+    Server = require('socket.io');
 
-//初始化
-var app = koa()
 
-var server = require('http').createServer(app.callback()),
-    io = require('socket.io')(server);
+function websocket(app){
+    var scfg = socket_config;
 
-// websocket connection
-// io.of('/blog')  破坏了路由
-io.on('connection', function(socket){
-	// socket.broadcast.emit('hi');
-	// io.emit('xxx', { 'data': msg });
-	// socket.on('xxx',function(msg){
-	// });
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-});
+    var srv = http.createServer(app.callback());
+    io = new Server(srv);
 
-// websocket emit something
-function wspush(name,msg){
-	io.emit(name, { 'data': msg });
+    // websocket emit something
+    function wspush(name, msg){
+    	io.emit(name, msg);
+    }
+
+    function wson(name, cb){
+        if (scfg[name]) return;
+        scfg[name] = cb;
+    }
+
+    function wsuse(cb){
+        return io.use(cb)
+    }
+
+    function robot(data){
+
+    }
+
+    global.SIO = {
+        on: wson,
+        emit: wspush,
+        use: wsuse,
+        robot: robot
+    }
+
+    return srv;
 }
 
-server.listen(7001);
+function of(path){
+    if (io){
+        io.of(path)
+        run()
+    }
+}
 
-module.exports = {}
+function run(){
+    function mkmkon(cb, skt){
+        return function(data){
+            cb.call({io: io}, data, skt)
+        }
+    }
+
+    function mkon(skt){
+        var scfg = socket_config;
+        var _keys = Object.keys(scfg)
+        _keys.map(function(item, i){
+            var _cb = mkmkon(scfg[item], skt)
+            skt.on(item, _cb)
+        })
+    }
+    if (io){
+        io.on('connection', function(socket){
+            mkon(socket);
+
+            // socket.on('imchat', function(data){
+            //     io.emit('imchat', {data: { user: 'world',message:'ni mei' }});
+            // })
+
+            socket.on('disconnect', function(){
+                console.log('user disconnect');
+            })
+        })
+    }
+}
+
+module.exports = {
+    init: websocket,
+    of: of,
+    run: run
+}
