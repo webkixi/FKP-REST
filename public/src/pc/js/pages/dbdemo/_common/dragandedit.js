d489580aba51bd91398f0d91c937165100553406
姚成 <kixi@163.com>
2016-06-12 23:51:03 +0800

    修复拖动的bug

--- public/src/pc/js/pages/dbdemo/_common/dragandedit.js
+++ public/src/pc/js/pages/dbdemo/_common/dragandedit.js
@@ -6,8 +6,10 @@ var _user = SA.get('USER')
 var _is_login = _user.error ? false : _user
 var is_touch = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch
 
-libs.inject()
-.js(['/js/t/jq/draggabilly.pkgd.min.js', '_Draggabilly'], lm)
+function startDrag(){
+    libs.inject()
+    .js(['/js/t/jq/draggabilly.pkgd.min.js', '_Draggabilly'], lm)
+}
 
 function lm(){
     if (_is_login){
@@ -67,8 +69,6 @@ function lm(){
         $('.lg_item')
         .append('<div class="dragstat del">删除</div>')
         .prepend('<div class="dragstat edit">修改</div>')
-
-        ($(ele));
 
         var $draggable = $(ele).draggabilly({
             axis: 'x'
@@ -159,4 +159,4 @@ function lm(){
 }
 
 
-module.exports = lm //listmethod
+module.exports = startDrag //listmethod
