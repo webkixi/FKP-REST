9f027fba64e8b494ba2bdbb3af78189404e0e7a2
姚成 <kixi@163.com>
2016-07-05 15:48:19 +0800

    首页加入tabs，调整博客首页结构

--- pages/dbdemo.js
+++ pages/dbdemo.js
@@ -4,7 +4,7 @@ var markdown = require('modules/markdown')
 var querystring = require('querystring')
 var mongoose = require("mongoose");
 
-function *index(oridata) {
+function *index(oridata, idx) {
     console.log('========= 列表页/'+__filename+' =========\n\n');
 
     var _this = this,
@@ -24,32 +24,25 @@ function *index(oridata) {
 
         // 处理文章详情数据
         if (location.query.topic){
-            try {
-                yield api.req(_this, '$counttopic')
+            yield api.req(_this, '$counttopic');
 
-                if (Cache.has(location.query.topic)){
-                    var tmp = Cache.peek(location.query.topic)
-                }
-                else{
-                    var tmp = yield return_detail()
-                }
-                var rtn = {
-                    isList: false,
-                    content: tmp.cnt.replace('h1','p'),
-                    mdmenu: tmp.mdmenu,
-                    title: tmp.ori.title,
-                    author: tmp.ori.user.username,
-                    create_at: tmp.ori.create_at
-                }
-                oridata.bloglist = rtn
-                oridata.title = tmp.ori.title
-
-            } catch (e) {
-                console.log('============ counttopic'+e);
+            if (Cache.has(location.query.topic)){
+                var tmp = Cache.peek(location.query.topic);
             }
+            else{
+                var tmp = yield return_detail();
+            }
+            var rtn = {
+                isList: false,
+                content: tmp.cnt.replace('h1','p'),
+                mdmenu: tmp.mdmenu,
+                title: tmp.ori.title,
+                author: tmp.ori.user.username,
+                create_at: tmp.ori.create_at
+            }
+            oridata.bloglist = rtn
+            oridata.title = tmp.ori.title
         }
-
-
         // 默认
         // 处理列表数据
         else {
@@ -68,17 +61,9 @@ function *index(oridata) {
     // =============== post ===================
 
     if (method === 'POST') {
-        if (this.session.$user) {
-            _user = this.session.$user;
-        }
 
-        if (!_user) {
-            return errors['10005'];
-        }
+        return yield api.req(_this, '$listtopic')
 
-        var topic = yield api.req(_this, '$listtopic')
-
-        return topic;
     }
 
 
@@ -87,46 +72,39 @@ function *index(oridata) {
     //使用react组件
     //数据要符合react/list组件的格式规范
     function *return_list(){
-
-        try {
-            var _props = {
-                // container: '',
-                // globalName: '_Pagi',
-                itemMethod: false,
-                listMethod: false,
-                itemClass: '',
-                listClass: 'pagenation wid-12',
-                data: {
-                    total: 40,
-                    per:   20,
-                    url:   '/',
-                    query: 'page='
-                },
-                begin: { start: 0, off: 5 }
-            }
-            var pagiHtml = yield react2html('react/modules/pagination/pagi', _props);
-            pagiHtml[0] = '<div class="pagi" id="pagi" >'+pagiHtml[0]+'</div>';
-
-
-            // 从数据库中获取数据
-            // '$' 为数据库api的前置标记，与前端一致
-            topics = yield api.req(_this, '$listtopic')
-            //整理数据，使数据符合 react 需求
-            topics = yield dealWith_topicsListData(topics)
-            // react组件解析生成html
-            // 传入 react 组件路径及数据
-            // var reactHtml = yield react2html('react/listView/list', {data: topics})
-            var reactHtml = yield react2html('react/widgets/listView/list', {data: topics})
-            reactHtml[0] = '<div class="load-list">'+reactHtml[0]+pagiHtml[0]+'</div>'
-
-
-            // 将html结构代码代入到oridata，用于模板渲染
-            return reactHtml[0]
-        } catch (e) {
-            console.log(e);
+        var _props = {
+            // container: '',
+            // globalName: '_Pagi',
+            itemMethod: false,
+            listMethod: false,
+            itemClass: '',
+            listClass: 'pagenation wid-12',
+            data: {
+                total: 40,
+                per:   20,
+                url:   '/',
+                query: 'page='
+            },
+            begin: { start: 0, off: 5 }
         }
-        //加入分页，优化seo
+        var pagiHtml = yield react2html('react/modules/pagination/pagi', _props);
+        pagiHtml[0] = '<div class="pagi" id="pagi" >'+pagiHtml[0]+'</div>';
 
+
+        // 从数据库中获取数据
+        // '$' 为数据库api的前置标记，与前端一致
+        topics = yield api.req(_this, '$listtopic')
+        //整理数据，使数据符合 react 需求
+        topics = dealWith_topicsListData(topics)
+        // react组件解析生成html
+        // 传入 react 组件路径及数据
+        // var reactHtml = yield react2html('react/listView/list', {data: topics})
+        var reactHtml = yield react2html('react/widgets/listView/list', {data: topics})
+        reactHtml[0] = '<div class="load-list">'+reactHtml[0]+pagiHtml[0]+'</div>'
+
+
+        // 将html结构代码代入到oridata，用于模板渲染
+        return reactHtml[0]
     }
 
 
@@ -142,7 +120,6 @@ function *index(oridata) {
 
         // console.log(cache_id);
 
-
         //整理数据，markdown 解析生成html
         var cnt = yield markdown(topics[0]['content'], {mdcontent:{ori: topics[0]}})
 
@@ -155,35 +132,13 @@ function *index(oridata) {
     }
 
     //整理数据使之符合react的List组件格式
-    function *dealWith_topicsListData(ttt){
+    function dealWith_topicsListData(ttt){
         var listdata_targ = []
         ttt.map(function(item){
-            // listdata_targ.push(item.title)
             listdata_targ.push(<a href={"?topic="+item._id}>{item.title}</a>)
         })
         return listdata_targ
     }
-
-    //整理数据使之符合react的List组件格式
-    function *dealWith_topicsDetailData(ttt){
-        var article = []
-        var item = ttt[0]
-
-        var h1_obj = ttt['cnt'].match(/<h1 ([^>]*)>(.*?)<\/h1>/)
-        var cnt = ttt['cnt'].replace(/<h1 ([^>]*)>(.*?)<\/h1>/, '')
-        var attrs = h1_obj[1].replace(' ','&')
-        attrs = querystring.parse(attrs)
-
-        article.push(
-            <div className="markdown">
-                <h1 id={attrs.id} className={attrs.class}>{item.title}</h1>
-                <small>作者：{item.user.username}</small>
-                {'~~mdcnt~~'}
-            </div>
-        )
-        return [article,cnt]
-    }
-
 }
 
 module.exports = {
--- public/src/pc/js/pages/dbdemo/index.jsx
+++ public/src/pc/js/pages/dbdemo/index.jsx
@@ -3,293 +3,42 @@
 
 
 SA.set('USER', {error: '-1'})
+var Tabs = require('modules/tabs/tabs');
+var IndexList = require('./_common/_index')
 
-var api = require('libs/api')
-var libs = require('libs/libs')
-// var AppList = require('modules/list/like_lagou');
-var listModule = require('modules/list/base_list');
 var WS = require('modules/wsocket/index')
 
-var _listData = []
-
-
-// var loginBox = require('modules/sign/signin')
-
 WS.on('imchat', function(data){
-    // libs.msgtips('有人在聊天室聊天哦','warning')
-    // libs.msgtips('去 /chat 看看情况','center')
     $('.chattip').show().addClass('animated tada')
     setTimeout(function(){
         $('.chattip').hide().removeClass('animated tada')
     },10000)
 })
 
-
-//异步调用js
-//webpack类似seajs的异步请求语法 require.ensure
-//有些文件需要异步调用
-require.ensure(['./_common/epic', './_common/dragandedit'], function(require){
-
-    // 页面url变量
-    var param = libs.queryString(),
-        repass = false,
-        cur_page = param.page ? param.page : 0,
-        p_tag = param.tag ? param.tag : null,
-        p_cat = param.cat ? param.cat : null;
-
-    if (param && param.type) {
-        var type = param.type;
-        if (type==='signup') {
-            repass = true;
-        }
-    }
-    else {
-        //初始化获取用户信息
-        //页面载入请求用户信息
-        api.req( '/$signin' )
-        .then(sign_resaults)
-
-        //注册信息
-        //signin返回信息回调
-        function sign_resaults(data){
-            SA.set('USER', data)
-            if (data.dbconfig){
-                libs.msgtips(data.dbconfig, 'alert')
-            }
-            if (data.error){ //没有该用户
-                // console.log(data);
-                SA.set('USER', {error: '-2'})
-            }
-            else{
-                $('#edit').click(function(){
-                    //设置按钮显示
-                    var txt = this.textContent;
-                    if (txt === '发布')
-                        this.textContent = '关闭'
-                    else
-                        this.textContent = '发布'
-
-                    $('body').trigger('openEditor')
-                })
-            }
-        }
-    }
-
-
-    //添加/更新文章
-    $('body').on('addTopic', function(e, args){
-        //添加文章 或者 修改文章
-        //ajax
-        var upid = false,
-            editor = args.editor;   // 编辑器
-        if (args.cnt.length){
-            if (libs.strLen(args.cnt)>15) {
-                if (args.upid){
-                    upid = args.upid
-                }
-                var postdata = {cnt: args.cnt};
-
-                //更新文章
-                if (upid){
-                    postdata.topic = upid;
-                    require('libs/api').req(
-                        '/$updatetopic',
-                        postdata,
-                        topic_resaults
-                    )
-                }
-
-                //添加文章
-                else {
-                    require('libs/api').req(
-                        '/$addtopic',
-                        postdata,
-                        topic_resaults
-                    )
-                }
-            }
-            else {
-                alert('文章字数少于15字')
-            }
-        }
-
-        function topic_resaults(data){
-            editor.importFile('')
-            $('.box').hide()
-            if (data.error){
-                libs.msgtips(data.message)
-            }
-            else{
-              if (upid){
-                  libs.msgtips('更新成功')
-              }
-              else {
-                  libs.msgtips('发布成功')
-              }
-            }
-            return data
-        }
-    })
-
-    //弹出编辑框
-    $('body').on('openEditor', function(jqevent, opts){
-        //打开输入框
-        $('.box').toggle()
-
-        if (opts && opts.content){
-          require('./_common/epic')(opts)   //类似seajs，按需异步请求
-        }
-        else{
-          //插入编辑器
-          //必须后置打开，不然编辑器的宽高不对
-          require('./_common/epic')()       //类似seajs，按需异步请求
-        }
-    })
-
-    $(window).scroll(function(){
-        var _top = $(window).scrollTop()
-        var _width = $(window).width()
-        if (_top>140){
-            $('.topper').addClass('fixed_top')
-            if (_width>1023){
-                $('.topper').css({'width': '68%'})
-            }
-            else {
-                $('.topper').css({'width': '98%'})
-            }
-            $('.side-menu').css({top: '3.5em'})
-        }
-        else {
-            $('.side-menu').css({top: '10.5em'})
-            $('.topper').removeClass('fixed_top')
-            $('.topper').css({'width': '100%'})
-        }
-    })
-
-    //打开/关闭编辑框
-    $('.box_close').click(function(){
-        $('body').trigger('openEditor')
-    })
-
-
-
-    //  ===========  列表文章  =========
-
-    // ajax方法
-    function pull_list_data(cb){
-        var login = SA.get('USER').login;
-        // pageNum, numPerPage, order
-        cur_page++
-        var params = {
-            page: cur_page,
-            tag: p_tag,
-            cat: p_cat
-        }
-        api.req('/$listtopic', params)
-        .then(function(data){
-            listTopic_resaults(data)
-            if (_.isFunction(cb)){
-                cb()
-            }
-        })
-    }
-
-
-    //ajax列表页数据处理
-    function listTopic_resaults(data){
-        _listData = [];
-
-        // 列表项
-        data.map(function(item, i){
-            var _title = (
-                <div className="title_header">
-                    <span><img src={item.user.avatar}/></span>
-                    <a href={"?topic="+item._id}>{item.title}</a>
-                    <abbr>
-                        {libs.timeAgo(item.create_at)}
-                    </abbr>
-                </div>
-            )
-
-            _listData.push({
-                title: _title,
-                body: [
-                    { k: '作者: ',
-                      v: item.user.nickname },
-                    { k: '标签: ',
-                      v: mk_tags(item.tags) },
-                    { k: '浏览: ',
-                      v: item.visit_count }
-                ]
-            })
-        })
-    }
-
-    // 加载列表数据
-    pull_list_data(function(){
-        if (!param.topic){
-            //列表页展示
-            $('#listtopic').html('')
-            $('#listtopic').css({'margin-left':0})
-
-            function itemFun(){
-            }
-
-            function listFun(){
-                require('./_common/dragandedit')()
-            }
-
-            //加载下一页数据
-            function loadMore(e, next){
-                listFun()
-                var next_page = cur_page + 1;
-                pull_list_data(function(){
-                    next(e, _listData)
-                })
-            }
-
-
-            listModule(_listData, {
-                container: 'listtopic',
-                globalName: 'Blogs',
-                listClass: 'like_lagou',
-                itemClass: 'lg_item',
-                listMethod: listFun,
-                itemMethod: itemFun,
-                scrollEnd: loadMore
-            })
-        }
-    })
-
-    // 生成标签模版
-    // tag
-    function mk_tags(tag){
-        if (!tag) return '';
-        if (!_.isArray(tag)) return [''];
-
-        var _v = _.map(tag, _.trim),
-            _tag = [];
-
-        _v.map(function($v){
-            _tag.push(<a href={'/?tag='+$v}>{$v}</a>)
-        })
-
-        return _tag;
-    }
-
-    // websocket 模块化调用
-    // var ws = require('modules/wsocket/index')
-    WS.on('article_count', function(val){
-        console.log(val.data);
-    })
-
-    $('img').each(function(i, item){
-        if ( item.src.indexOf('http://www.agzgz')===-1 &&
-             item.src.indexOf('uploader')>-1
-        ){
-            var _src = item.src.substr(item.src.indexOf('uploader'))
-            item.src = 'http://www.agzgz.com/'+_src
-        }
-    })
-
+WS.on('article_count', function(val){
+    console.log(val.data);
 })
+
+
+var _config = [
+    '博客',
+    'FKPJS',
+    {title: <a href="/app">大单页</a>, 'data-href':'大单页'},
+    {title: <a href="/chat">聊天室</a>, 'data-href':'聊天室'},
+    {title: <a href="/cat">分类列</a>, 'data-href':'分类列'},
+    {title: <a href="/demotabs">Tabs</a>, 'data-href':'Tabs'},
+    {title: <a href="/formshow">表单</a>, 'data-href':'表单'},
+    {title: <a href="/hello">接口</a>, 'data-href':'接口'},
+    {title: <a href="/pagi">分页</a>, 'data-href':'分页'},
+    {title: <a href="/ueditor">编辑器</a>, 'data-href':'编辑器'}
+]
+
+var ttt = Tabs(_config,
+    {   container: 'forTabs',
+        globalName: 'TabSwitch'
+    },
+    function(views){
+        IndexList(views[0]);
+        views[1].content( "这其实是tabs的实例" )
+    }
+)
