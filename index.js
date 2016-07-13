b4a66bf1bae594b39d98d763772494ad3f965a18
姚成 <kixi@163.com>
2016-07-14 01:07:09 +0800

    优化node首页index.js，beautiful code

--- index.js
+++ index.js
@@ -1,72 +1,67 @@
 /*
- * author: ralf
- * ly nodejs mvc project
+ * FKPJS - Full Stack Fragment
+ * author: 天天修改
  */
 
+// 环境依赖
 require('babel-core/register');
 require("babel-polyfill");
-//配置环境路径
-var path = require('path');
-var base = path.resolve(__dirname);
-require('./modules/requirePath')(base);
+global._ = require('lodash');
+require('./modules/requirePath')(require('path').resolve(__dirname));
 
+var port = 8070;
+var args = process.argv.splice(2); //取得命令行参数
+if( args[0] && (args[0] === 'test' ||
+	args[0].indexOf('env_')>-1 ) ) {
+		process.env.whereIs = 'pro'
+		process.env.env = args[0]
+}
+else if( args[1] && (args[1] === 'test' ||
+	args[1].indexOf('env_')>-1) ) {
+		process.env.whereIs = args[0]
+		process.env.env = args[1]
+}
+global.fkpConfig = require('./config')(process.env.env);   // 全局config
+
+
+
+var app = require('koa')();
+var cors = require('kcors');   //cros 跨域支持
+var session = require('koa-generic-session');
 global.React = require('react');
 global.ReactDomServer = require('react-dom/server');
-global._ = require('lodash');
+process.env.env = 'default';
+require('./modules/cache')  // lru 缓存模块
 
-var args = process.argv.splice(2); //取得命令行参数
 
-process.env.env = 'default'
-if( args[0] && (args[0] === 'test' || args[0].indexOf('env_')>-1 ) ){
-	process.env.whereIs = 'pro'
-	process.env.env = args[0]
-}
-else
-if( args[1] && (args[1] === 'test' || args[1].indexOf('env_')>-1) ) {
-	process.env.whereIs = args[0]
-	process.env.env = args[1]
-}
-// 全局config
-global.fkpConfig = require('./config')(process.env.env)
 
-var koa = require('koa'),
-	session = require('koa-generic-session');
 
 //自定义部分模块
-require('./modules/cache')  // lru 缓存模块
 var statics = require('./modules/static'),
-	_mapper = require('./modules/mapper')(args[0]),
 	route = require('./modules/route'),
 	socketio = require('./modules/wsocket'),   //websocket
 	render = require('./modules/render'),
-	cors = require('kcors');    //cros 跨域支持
+	_mapper = require('./modules/mapper')(args[0]);
+
 
 
 //初始化
-var app = koa();
 app.use(cors());
 
 //静态资源 js css
 statics(args[0], app)
 
-
 //session
 app.keys = ['keys','gzgzmixcookie'];
-app.use(session({
-	key: 'agzgz'
-}));
+app.use(session( { key: 'agzgz' } ));
 
 
 // websocket 初始化
 var server = socketio.init(app)
-
 //router
-// app.use(router(app)); //开启路由
 route.call(this,app,_mapper,render(args[0]))
-
 //websocket 挂载on
 socketio.run();
-
 
 
 //打印出错信息
@@ -74,22 +69,11 @@ app.on('error', function(err){
     console.log(err);
 });
 
-var port = 8070;
 if(args[1]){
 	if (/[\d]+/.test(args[1])){
 		port = args[1]
 	}
 }
 
-// app.listen(port);
 server.listen(port);
-
-
-// var exec = require('child_process').execSync;
-// var spawn = require('child_process').spawn,
-// // ls = spawn('ttt');
-// exec('source ./ttt')
-
-// var exec = require('child_process').execSync;
-// // var cmd = 'nohup node --harmony ../index.js dev &'
-// exec('source ttt');
+// app.listen(port);
