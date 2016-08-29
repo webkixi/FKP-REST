fac581ad4b77cc04a3571d41b21848d32e3198d2
姚成 <kixi@163.com>
2016-08-30 01:13:43 +0800

    树形菜单组件完善

---
+++ public/react/modules/menutree/_menutree/index.jsx
@@ -0,0 +1,101 @@
+var List = require('react/widgets/listView/list');
+
+function adapter(data){
+	function getTitile(title){
+		return (
+			<h4>
+				<i className="iconfont icon-xiala"/>
+				<span className="catalog">{title}</span>
+			</h4>
+		)
+	}
+
+	function getLis(lis){
+		var _lis = [];
+		if (lis.length){
+			lis.map(function(item, i){
+				_lis.push({
+					title: item.title,
+					url: item.url
+				})
+			})
+		}
+		return _lis
+	}
+
+	var childrenCount = 1;   //限制级数为3层, react不能超过3层
+	function getChildren(father, cls){
+		childrenCount++
+		var _tmp = [];
+		father.children.map(function(child){
+			_tmp.push({
+				title: getTitile(data[child].caption),
+				li: getLis(data[child].list),
+				className: cls||''
+			})
+			if (childrenCount<3){
+				if (data[child].children.length){
+					_tmp.push( getChildren(data[child]) )
+				}
+			}
+		})
+		return _tmp
+	}
+
+	var keys = Object.keys(data);
+	var _data = [];
+	keys.map(function(item){
+		if (data[item].parent==='root'){
+			if (data[item].children.length && item!=='root'){
+				childrenCount = 1;
+				var lis = getLis(data[item].list);
+				var childs = getChildren(data[item]);
+				_data.push({
+					title: getTitile(data[item].caption),
+					li: lis.concat(childs)
+				})
+			}
+			else {
+				_data.push({
+					title: getTitile(data[item].caption),
+					li: getLis(data[item].list)
+				})
+			}
+		}
+	})
+	return _data;
+}
+
+var pagenation = {
+	//插入真实 DOM之前
+	componentWillMount:function(){
+        this.setState({
+			data: adapter(this.props.data)
+		})
+
+        this.data = adapter(this.props.data);
+	},
+
+    componentDidMount:function(){ },
+
+	render:function(){
+        var list;
+        if (this.state.data.length){
+			// var _props = _.merge({data: this.data}, this.props);
+			// list = React.createElement(List, _props)
+            list = <List data={this.data} itemClass="category" listMethod={this.props.listMethod} itemMethod={this.props.itemMethod}/>
+        }
+        return (
+            <div className="page-ui-kits docmenu">
+                {list}
+            </div>
+        )
+	}
+}
+
+
+function actRct( storeName ){
+    return require('react/util')(storeName, pagenation)
+}
+
+module.exports = actRct;
---
+++ public/react/modules/menutree/index.jsx
@@ -0,0 +1,47 @@
+var MenuTree = require('./_menutree/index');
+var render = React.render;
+
+function menuTree(data, opts ){
+
+    var noop = false,
+        dft = {
+            container: '',
+            globalName: '_Mtree',
+            itemMethod: noop,
+            listMethod: noop,
+            itemClass: '',
+            listClass: 'pagenation wid-12',
+            data: {
+                total: 200,
+                per:   10,
+                url:   '/',
+                query: 'page='
+            },
+            begin: { start: 0, off: 7 }
+        };
+
+        dft = _.assign(dft, opts)
+
+    if (!dft.container) return false;
+    if (data) {
+        dft.data = data
+    }
+
+    // SAX.set(dft.globalName, {
+    //     data: data,
+    //     begin: dft.begin
+    // })
+
+    var Mt = MenuTree(dft.globalName)
+
+    render(
+        <Mt itemDefaultMethod={idm} itemMethod={dft.itemMethod} listMethod={dft.listMethod} itemClass={dft.itemClass} listClass={dft.listClass}/>,
+        document.getElementById(dft.container)
+    )
+}
+
+menuTree.pure = function(){
+    return MenuTree()
+};
+
+module.exports = menuTree
