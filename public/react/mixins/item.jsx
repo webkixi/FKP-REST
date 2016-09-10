81ea6ede710427162f6502dd98f7a6d9f4bcccfb
yc <kixi@163.com>
2016-09-11 03:14:34 +0800

    core-优化itemmixin，itemDefaultMethod的call对象错误

--- public/react/mixins/item.jsx
+++ public/react/mixins/item.jsx
@@ -7,41 +7,38 @@ function itemMixin(){
     return {
         getInitialState: function() {
     		return {
-            	params: '',
-                icon: '',
-                iconPre: ''
+          	params: '',
+            icon: '',
+            iconPre: ''
     	    };
     	},
-        componentWillMount: function(){
-            this.intent = this.props.intent || [];
-            this.intent.ctx = this;
-        },
-        componentDidMount: function() {
-            //React.findDOMNode(this) // var box = React.findDOMNode(this.refs.popbox);
-            var self = this;
-            var that = React.findDOMNode(this);
+      componentWillMount: function(){
+          this.intent = this.props.intent || [];
+          this.intent.ctx = this;
+      },
+      componentDidMount: function() {
+          //React.findDOMNode(this) // var box = React.findDOMNode(this.refs.popbox);
+          var self = this;
+          var that = React.findDOMNode(this);
 
-            if(this.props.itemDefaultMethod){
-                if(this.props.itemMethod){
-                    var mtd = this.props.itemMethod;
-                    mtd.call(that, self.intent);
-                }
-                setTimeout(function(){
-                    var dMtd = self.props.itemDefaultMethod;
-        			if(typeof dMtd==='function'){
-        				dMtd.call(that, this.intent);
-        			}
-                }, 17)
-    		}
-            else
+          if(this.props.itemDefaultMethod){
             if(this.props.itemMethod){
                 var mtd = this.props.itemMethod;
                 mtd.call(that, self.intent);
             }
-
-
-
-        }
+            setTimeout(function(){
+              var dMtd = self.props.itemDefaultMethod;
+        			if(typeof dMtd==='function'){
+        				dMtd.call(that, self.intent);
+        			}
+            }, 17)
+	        }
+          else
+          if(this.props.itemMethod){
+              var mtd = this.props.itemMethod;
+              mtd.call(that, self.intent);
+          }
+      }
     }
 }
 
